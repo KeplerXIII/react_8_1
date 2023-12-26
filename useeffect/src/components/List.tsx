8e63d72c164beb5fc534ad26cb22dd106e5ed7c8
keplerxiii <a.v.pushkin@gmail.com>
2023-12-26 18:25:07 +0300

    upd

--- useeffect/src/components/List.tsx
+++ useeffect/src/components/List.tsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { User } from '../models'
 import { Details } from './Details'
-import { useFetch } from '../hooks/useFetch'
 
 export const List = () => {
   const apiUrl =
     'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json'
 
-  const { data, isLoading, hasError } = useFetch(apiUrl)
+  const [data, setData] = useState<User[]>()
+  const [isLoading, setLoading] = useState<boolean>(false)
+  const [hasError, setError] = useState<Error | null>(null)
   const [selectedUser, setSelectedUser] = useState<User | null>(null)
+  const timestampRef = useRef<number>()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const timestamp = Date.now()
+      timestampRef.current = timestamp
+      setLoading(true)
+
+      try {
+        const response = await fetch(apiUrl)
+
+        if (!response.ok) {
+          throw new Error(response.statusText)
+        }
+
+        const users: User[] = await response.json()
+
+        if (timestampRef.current === timestamp) {
+          setData(users)
+        }
+
+        setError(null)
+      } catch (e) {
+        if (e instanceof Error) setError(e)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
 
   const handleButtonClick = (user: User) => {
     setSelectedUser(user)
